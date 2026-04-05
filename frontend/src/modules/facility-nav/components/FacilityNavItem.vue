598bd154678c0eff00bec946572407cbe8b18c6b
ReniX99 <oleg.burkov99@gmail.com>
2026-04-05 19:39:08 +0300

    feat(facility-nav)-frontend: add facility navigation

--- frontend/src/modules/facility-nav/components/FacilityNavItem.vue
+++ frontend/src/modules/facility-nav/components/FacilityNavItem.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
   defineProps<{
     iconUrl: string
+    routerName: string
+    currentRouterName?: string
   }>()
 </script>
 
 <template>
-  <li class="w-15 h-12 rounded-t-[15px] flex justify-center items-center cursor-pointer">
-    <img :src="iconUrl" alt="Icon" />
-  </li>
+  <router-link
+    :class="[
+      'rounded-t-[15px]',
+      { 'scale-y-125 origin-bottom ': routerName === currentRouterName },
+    ]"
+    :to="{ name: routerName }"
+  >
+    <li
+      :class="[
+        'w-15 h-12 flex justify-center items-center cursor-pointer',
+        { 'scale-y-80': routerName === currentRouterName },
+      ]"
+    >
+      <img :src="iconUrl" alt="Icon" />
+    </li>
+  </router-link>
 </template>
--- frontend/src/modules/facility-nav/components/FacilityNavList.vue
+++ frontend/src/modules/facility-nav/components/FacilityNavList.vue
@@ -4,13 +4,38 @@
   import chatSvg from '@/assets/svg/facility-nav/chat.svg'
   import requestSvg from '@/assets/svg/facility-nav/request.svg'
   import docsSvg from '@/assets/svg/facility-nav/docs.svg'
+  import { useRoute } from 'vue-router'
+  import { computed } from 'vue'
+
+  const route = useRoute()
+  const currentRouterName = computed(() => route.name?.toString())
 </script>
 
 <template>
   <ul class="mt-8 ml-29 flex gap-3">
-    <FacilityNavItem :icon-url="infoSvg" class="bg-[#351303]" />
-    <FacilityNavItem :icon-url="chatSvg" class="bg-[#072768]" />
-    <FacilityNavItem :icon-url="requestSvg" class="bg-[#996F51]" />
-    <FacilityNavItem :icon-url="docsSvg" class="bg-[#5079B2]" />
+    <FacilityNavItem
+      :icon-url="infoSvg"
+      router-name="facility"
+      :current-router-name="currentRouterName"
+      class="bg-[#351303]"
+    />
+    <FacilityNavItem
+      :icon-url="chatSvg"
+      router-name="facility-chats"
+      :current-router-name="currentRouterName"
+      class="bg-[#072768]"
+    />
+    <FacilityNavItem
+      :icon-url="requestSvg"
+      router-name=""
+      :current-router-name="currentRouterName"
+      class="bg-[#996F51]"
+    />
+    <FacilityNavItem
+      :icon-url="docsSvg"
+      router-name=""
+      :current-router-name="currentRouterName"
+      class="bg-[#5079B2]"
+    />
   </ul>
 </template>
